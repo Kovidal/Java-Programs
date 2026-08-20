af7a0d8b354d049131dd842d8adefb42861864dd
Kovida Luthra <kovidaluthra13@gmail.com>
2026-08-20 10:01:41 +0530

    updted code

--- JavaProgramming/src/Module2/Q1/utilities/MathUtils.java
+++
@@ -1,8 +0,0 @@
-package utilities;
-
-public class MathUtils {
-
-    public int add(int a, int b) {
-        return a + b;
-    }
-}
--- JavaProgramming/src/Module2/Q2/shapes/Shape.java
+++
@@ -1,8 +0,0 @@
-package shapes;
-
-public interface Shape {
-
-    double area();
-
-    double perimeter();
-}
